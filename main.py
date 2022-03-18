dbf567f71b11ec2773933251abe5dce4c27ae995
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-18 17:36:53 +0000

    solution day 10

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from art import logo
+
 # Add
 def add(n1, n2):
   return n1 + n2
@@ -22,9 +24,11 @@ operations = {
   "/": divide
 }
 
+# Calculator Function
 def calculator():
+  print(logo)
   # Ask for numbers
-  num1 = int(input("What's the first number: "))
+  num1 = float(input("What's the first number: "))
   # Loop through operations
   for symbol in operations:
     print(symbol)
@@ -33,17 +37,18 @@ def calculator():
   
   while should_continue:
     operation_symbol = input("Pick an operation from the selection above: ")
-    num2 = int(input("What's the second number: "))
+    num2 = float(input("What's the second number: "))
     # Get the operation symbol from operations
     calculation_function = operations[operation_symbol]
     answer = calculation_function(num1, num2)
   
     print(f"{num1} {operation_symbol} {num2} = {answer}")
   
-    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == "y":
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to end calculation: ") == "y":
       num1 = answer
     else:
       should_continue = False
-      # calculator()
+      if input("Type 'y' if you like to calculate new numbers?  or 'n' to exit") == "y":
+        calculator()
 
 calculator()
