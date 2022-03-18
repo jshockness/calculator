a295195d68aeab4ea3e373dd0883775039dbf641
Jason Shockness <51177417+jshockness@users.noreply.github.com>
2022-03-18 17:21:02 +0000

    calc function

--- main.py
+++ main.py
@@ -22,22 +22,28 @@ operations = {
   "/": divide
 }
 
-# Ask for numbers
-num1 = int(input("What's the first number: "))
-# Loop through operations
-for symbol in operations:
-  print(symbol)
-operation_symbol = input("Pick an operation from the selection above: ")
-num2 = int(input("What's the second number: "))
-# Get the operation symbol from operations
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
+def calculator():
+  # Ask for numbers
+  num1 = int(input("What's the first number: "))
+  # Loop through operations
+  for symbol in operations:
+    print(symbol)
+  # create while loop to check if user wants to proceed.
+  should_continue = True
+  
+  while should_continue:
+    operation_symbol = input("Pick an operation from the selection above: ")
+    num2 = int(input("What's the second number: "))
+    # Get the operation symbol from operations
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+  
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+  
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ") == "y":
+      num1 = answer
+    else:
+      should_continue = False
+      # calculator()
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
-
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the next number: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(first_answer, num3)
-
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+calculator()
